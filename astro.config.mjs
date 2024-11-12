bd76ddb9af88c19e9176da76a2fcf98a0ce85def
ali tarichi <tarichiali@gmail.com>
2024-11-12 17:43:00 +0300

    inital

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
+	type: "content",
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
@@ -9,8 +9,25 @@ const blog = defineCollection({
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		category: z.string(),
+		author: z.object({
+			name: z.string(),
+			title: z.string(),
+			image: z.string(),
+		}),
 	}),
 });
 
-export const collections = { blog };
+const careers = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		category: z.string(),
+		remote: z.boolean(),
+		type: z.string(),
+	}),
+});
+
+export const collections = { blog, careers };
+
