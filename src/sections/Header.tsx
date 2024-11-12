2c1dc27c1e464fb43257caad19528932581a4f3d
ali tarichi <tarichiali@gmail.com>
2024-11-12 18:18:35 +0300

    header section fixed

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -9,7 +9,7 @@ export const HeaderSection = () => {
           <div className="flex gap-4 items-center">
             <button
               className="bg-fuchsia-500/20 px-4 py-2 font-extrabold
-            uppercase font-heading text-sm tracking-wide relative"
+            uppercase font-heading text-sm tracking-wide relative hidden md:inline-flex"
             >
               <div
                 className="absolute inset-0 outline outline-2 -outline-offset-2
