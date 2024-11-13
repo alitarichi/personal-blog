3e0151b1bea49d21bbcbf7434f5babd6a96df652
ali tarichi <tarichiali@gmail.com>
2024-11-13 11:17:16 +0300

    hero section init

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -27,7 +27,7 @@ export const HeaderSection = () => {
                 <path
                   d="M0 1H12.2667L23 11.7333V24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 ></path>
               </svg>
               <span className="leading-6">Get Started</span>
