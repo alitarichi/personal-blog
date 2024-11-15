4c783373ed7650b989a5c483cda89899b73b52e5
ali tarichi <tarichiali@gmail.com>
2024-11-15 17:58:12 +0300

    card component added

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -1,12 +1,32 @@
 export const LatestPosts = () => {
   return (
-    <section>
+    <section className="py-60">
       <div className="container">
-        <h2>Your Portal to everything blockChain</h2>
-        <p>
+        <h2 className="font-heading font-black text-4xl text-center">
+          Your Portal to everything blockChain
+        </h2>
+        <p className="text-xl text-center text-zinc-400 mt-8">
           keep up with the newest trends, updates and insights in the blockchain
           world, updated weekly
         </p>
+        <div className="mt-16">
+          {[
+            ...new Array(4).fill(0).map((item, itemIndex) => (
+              <div>
+                <div>Technology</div>
+                <h3>Regulatory Challenges Facing Blockchain</h3>
+                <p>
+                  Understanding the regulatory landscape surrounding blockchain
+                  and what it means for the future of this technology.
+                </p>
+                <div>
+                  <button>Read More</button>
+                  <div>arrow</div>
+                </div>
+              </div>
+            )),
+          ]}
+        </div>
       </div>
     </section>
   );
