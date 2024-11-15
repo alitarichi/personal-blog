ad4c97cb103a350a02753c41e41b60a240e11749
ali tarichi <tarichiali@gmail.com>
2024-11-15 17:19:10 +0300

    latest posts init

---
+++ src/sections/LatestPosts.tsx
@@ -0,0 +1,13 @@
+export const LatestPosts = () => {
+  return (
+    <section>
+      <div className="container">
+        <h2>Your Portal to everything blockChain</h2>
+        <p>
+          keep up with the newest trends, updates and insights in the blockchain
+          world, updated weekly
+        </p>
+      </div>
+    </section>
+  );
+};
