c5491431263ad43f9c4a3610440a340662d0b739
ali tarichi <tarichiali@gmail.com>
2024-11-15 18:10:32 +0300

    latest post design implanted

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -1,3 +1,5 @@
+import { Card } from "../components/Card";
+
 export const LatestPosts = () => {
   return (
     <section className="py-60">
@@ -9,21 +11,21 @@ export const LatestPosts = () => {
           keep up with the newest trends, updates and insights in the blockchain
           world, updated weekly
         </p>
-        <div className="mt-16">
+        <div className="mt-16 flex flex-col gap-8">
           {[
             ...new Array(4).fill(0).map((item, itemIndex) => (
-              <div>
-                <div>Technology</div>
-                <h3>Regulatory Challenges Facing Blockchain</h3>
-                <p>
+              <Card key={itemIndex} buttonText="Read More">
+                <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
+                  Technology
+                </div>
+                <h3 className="font-heading font-black text-3xl mt-4">
+                  Regulatory Challenges Facing Blockchain
+                </h3>
+                <p className="text-lg text-zinc-400 mt-6">
                   Understanding the regulatory landscape surrounding blockchain
                   and what it means for the future of this technology.
                 </p>
-                <div>
-                  <button>Read More</button>
-                  <div>arrow</div>
-                </div>
-              </div>
+              </Card>
             )),
           ]}
         </div>
