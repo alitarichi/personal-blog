5691279c852ac9dd1b4d7d4e13f042f79b39169d
ali tarichi <tarichiali@gmail.com>
2024-11-19 12:24:41 +0300

    testimonials init

---
+++ src/sections/Testimonials.tsx
@@ -0,0 +1,35 @@
+export const TestimonialsSection = () => {
+  return (
+    <section className="py-32 bg-zinc-800">
+      <div className="container">
+        <div>
+          {[...new Array(3)].fill(0).map((_, index) => (
+            <div>
+              <blockquote>
+                <p className="font-heading text-3xl font-black">
+                  &ldquo; The user experience is phenomenal, and the support
+                  team is always there to help. Highly Recommended!&rdquo;
+                </p>
+                <cite className="mt-8 block">
+                  <div className="flex">
+                    <div>
+                      <div className="size-16 bg-zinc-700 rounded-full ">
+                        avatar
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-lg not-italic font-black">
+                    Erica Wyatt
+                  </div>
+                  <div className="text-zinc-400 not-italic">
+                    Product Manager - BlockLink
+                  </div>
+                </cite>
+              </blockquote>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
