28419ea894f618ad8d29244c6ee36ad13af18dd6
ali tarichi <tarichiali@gmail.com>
2024-11-20 17:11:50 +0300

    latestPosts aimation added

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -4,11 +4,21 @@ import { Tag } from "../components/Tag";
 import { getPostColorFromCategory } from "../utils/postUtils";
 import { CutCornerButton } from "../components/CutCornerButton";
 import { twMerge } from "tailwind-merge";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const LatestPosts = (props: {
   latestPosts: CollectionEntry<"blog">[];
 }) => {
   const { latestPosts } = props;
+
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "start center"],
+  });
+
+  const marginTop = useTransform(scrollYProgress, [0, 1], [0, 64]);
   return (
     <section className="py-60">
       <div className="container">
@@ -44,7 +54,11 @@ export const LatestPosts = (props: {
               )
             )}
           </div>
-          <div className="hidden md:flex flex-col gap-8 mt-16">
+          <motion.div
+            className="hidden md:flex flex-col gap-8 mt-16"
+            ref={targetRef}
+            style={{ marginTop }}
+          >
             {latestPosts.map(
               ({ data: { title, description, category } }, postIndex) => (
                 <Card
@@ -65,7 +79,7 @@ export const LatestPosts = (props: {
                 </Card>
               )
             )}
-          </div>
+          </motion.div>
         </div>
         <div className="flex justify-center mt-48 md:mt-32">
           <CutCornerButton>Read The Blog</CutCornerButton>
